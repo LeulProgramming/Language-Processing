cdb2d3f20a1a23d727a2933071d2279d536c47c2
leulprogramming <63164236+LeulProgramming@users.noreply.github.com>
2023-12-01 17:35:09 -0600

    Initial Commit!

---
+++ main.py
@@ -0,0 +1,58 @@
+import cohere
+from data import obtain_data
+from secret import API_KEY
+
+from cohere.responses.classify import Example
+co = cohere.Client(API_KEY)
+
+CLASSIFICATIONS = [
+    "positive review",
+    "neutral review",
+    "negative review"
+]
+
+examples=[
+    Example("The order came 5 days early", CLASSIFICATIONS[0]),
+    Example("The item exceeded my expectations", CLASSIFICATIONS[0]),
+    Example("I ordered more for my friends", CLASSIFICATIONS[0]),
+    Example("I would buy this again", CLASSIFICATIONS[0]),
+    Example("I would recommend this to others", CLASSIFICATIONS[0]),
+    Example("This product is awesome", CLASSIFICATIONS[0]),
+    Example("The package was damaged", CLASSIFICATIONS[2]),
+    Example("The order is 5 days late", CLASSIFICATIONS[2]),
+    Example("The order was incorrect", CLASSIFICATIONS[2]),
+    Example("I want to return my item", CLASSIFICATIONS[2]),
+    Example("The item's material feels low quality", CLASSIFICATIONS[2]),
+    Example("The product was okay", CLASSIFICATIONS[1]),
+    Example("I received five items in total", CLASSIFICATIONS[1]),
+    Example("I bought it from the website", CLASSIFICATIONS[1]),
+    Example("I used the product this morning", CLASSIFICATIONS[1]),
+    Example("The product arrived yesterday", CLASSIFICATIONS[1])
+]
+
+inputs=[
+  "Confirm your email address",
+  "hey i need u to send some $",
+]
+
+def print_classifications(input):
+    response = co.classify(
+        inputs = input,
+        examples=examples,
+    )
+
+    counts = [0] * len(CLASSIFICATIONS)
+    totalCount = 0;
+    for classifcation in response: 
+        for i in range(len(CLASSIFICATIONS)):
+            label = CLASSIFICATIONS[i]
+            if classifcation.predictions[0] == label:
+                counts[i] += 1
+                totalCount += 1
+
+    for i in range(len(CLASSIFICATIONS)):
+        print(f"{CLASSIFICATIONS[i]} {counts[i]/totalCount}")
+
+
+inputs = obtain_data()
+print_classifications(inputs)
